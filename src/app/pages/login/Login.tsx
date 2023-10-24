89d1d1f8f4c1be1f15a9ba0d23516fcf497c6feb
Thaliaraujo <thalia.araujo321@gmail.com>
2023-10-24 19:01:35 -0300

    feat: add login page

---
+++ src/app/pages/login/Login.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+    return(
+        <>
+            <p>Login</p>
+            <Link to="/pagina-inicial">Página Inicial</Link>
+        </>
+    );
+};
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const Rotas = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/pagina-inicial" element={<Dashboard />}/>
+                <Route path="/login" element={<Login />}/>
+
 
                 <Route path="*" element={<Navigate to='/pagina-inicial'/>} />
             </Routes>
