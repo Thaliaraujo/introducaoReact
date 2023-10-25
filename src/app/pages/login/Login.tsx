5a4df82495be7d335df3b49810349b366a49cd3e
Thaliaraujo <thalia.araujo321@gmail.com>
2023-10-25 10:59:41 -0300

    feat: add login form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,10 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleLogin = () => {
+        console.log(email);
+        console.log(password);
+    };
+
     return(
-        <>
-            <p>Login</p>
-            <Link to="/pagina-inicial">Página Inicial</Link>
-        </>
+        <div>
+            <form>
+
+                <label>
+                    <span>Email</span>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+
+                <button type="button" onClick={handleLogin}>
+                    Entrar
+                </button>
+            </form>
+        </div>
     );
 };
