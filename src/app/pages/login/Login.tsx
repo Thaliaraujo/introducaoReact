4d742c6812cbe8c0aa0bd20ad9175092c4f078b0
Thaliaraujo <thalia.araujo321@gmail.com>
2023-10-25 14:22:21 -0300

    feat: add component inputLogin and props

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,32 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
+import { InputLogin } from "./components/InputLogin";
 
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
-    const handleLogin = () => {
+    useEffect(() => {
+        console.log(email)
+    }, [email]);
+
+    useEffect(() => {
+        console.log(password)
+    }, [password]);
+
+    const emailLength = useMemo(() => {
+        console.log("executado");
+        return email.length * 1000;
+    }, [email.length]);
+
+    const handleLogin = useCallback(() => {
         console.log(email);
         console.log(password);
-    };
+    }, [email, password])
 
     return(
         <div>
             <form>
+                <p>Quantidade de caracteres do email: {emailLength}</p>
+                
+                <InputLogin 
+                    label="Email" 
+                    value={email} 
+                    onChange={newValue => setEmail(newValue)}
+                />
 
-                <label>
-                    <span>Email</span>
-                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-                </label>
+                <InputLogin 
+                    label="Senha" 
+                    type="password"
+                    value={password} 
+                    onChange={newValue => setPassword(newValue)}
+                />
 
-                <label>
-                    <span>Senha</span>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-                </label>
-
-                <button type="button" onClick={handleLogin}>
+                <button 
+                    type="button" 
+                    onClick={handleLogin}
+                >
                     Entrar
                 </button>
+
             </form>
         </div>
     );
